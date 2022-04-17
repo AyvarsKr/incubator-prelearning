e543468e229598452c7987be502ab99c5337ea75
AyvarsKr <ayvars@bk.ru>
2022-04-18 00:12:15 +0300

    Added Component Star

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Star from './star'
+
+function App() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    );
+}
+
+export default App;
